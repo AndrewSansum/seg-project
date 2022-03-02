0055da21e5ba42d40040c5958a3b6445628da5fb
Guillaume CV <gcv1u20@soton.ac.uk>
2022-03-02 16:06:15 +0000

    full project

--- src/main/java/com/example/segproject/App.java
+++ src/main/java/com/example/segproject/App.java
@@ -4,6 +4,11 @@ package com.example.segproject;
  * Hello world!
  *
  */
+
+/**
+ * Handle inputs
+ * Output [Data type]. Find that data type
+ */
 public class App 
 {
     public static void main( String[] args )
