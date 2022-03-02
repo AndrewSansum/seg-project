85ed74c5e113724a68bf13c099f878c3440167b9
Guillaume CV <gcv1u20@soton.ac.uk>
2022-03-02 17:52:14 +0000

    Getters/Setters for Canvas

---
+++ src/main/java/com/example/segproject/Canvas.java
@@ -0,0 +1,33 @@
+package com.example.segproject;
+
+/**
+ * Class where you'll find methods to
+	 * getters/setters a 3D tile map from runway and obstacles infos (input format to be defined)
+	 * getters/setters top-down view from the 3D tile map
+	 * getters/setters side view from the 3D tile map
+ */
+public class Canvas 
+{
+	private int[][][]	tileMap3D;
+	private int[][]		tileMapSideView;
+	private int[][]		tileMapTopDownView;
+
+	public void			setTileMap3D() {
+		;
+	}
+	public void			setTileMapSideView() {
+		;
+	}
+	public void			setTileMapTopDownView() {
+		;
+	}
+	public int[][][]	getTileMap3D() {
+		return (this.tileMap3D);
+	}
+	public int[][]		getTileMapSideView() {
+		return (this.tileMapSideView);
+	}
+	public int[][]		getTileMapTopDownView() {
+		return (this.tileMapTopDownView);
+	}
+}
