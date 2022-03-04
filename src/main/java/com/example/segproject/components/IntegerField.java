70ac3d2cc655d8ce2b48bbe8760a29053577b53c
Andrew Sansum <sansumandrew@gmail.com>
2022-03-04 20:32:11 +0000

    Change IntegerField to only accept positive values

--- src/main/java/com/example/segproject/components/IntegerField.java
+++ src/main/java/com/example/segproject/components/IntegerField.java
@@ -2,6 +2,7 @@ package com.example.segproject.components;
 
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextFormatter;
+import javafx.util.StringConverter;
 import javafx.util.converter.IntegerStringConverter;
 
 /**
@@ -10,6 +11,22 @@ import javafx.util.converter.IntegerStringConverter;
 public class IntegerField extends TextField {
     public IntegerField() {
         super();
-        setTextFormatter(new TextFormatter<Integer>(new IntegerStringConverter()));
+        setTextFormatter(new TextFormatter<Integer>(new PositiveIntegerStringConverter()));
+    }
+
+    private class PositiveIntegerStringConverter extends StringConverter<Integer> {
+        @Override
+        public Integer fromString(String str) {
+            return Math.abs(Integer.parseInt(str));
+        }
+
+        @Override
+        public String toString(Integer num) {
+            String outStr = "";
+            if (num != null && num >= 0) {
+                outStr = num.toString();
+            }
+            return outStr;
+        }
     }
 }
