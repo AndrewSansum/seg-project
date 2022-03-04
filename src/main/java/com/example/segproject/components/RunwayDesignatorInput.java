a7f9838c2f0e0b3177e5ba93aee8f9ed617bbbc4
Andrew Sansum <sansumandrew@gmail.com>
2022-03-04 19:34:42 +0000

    Add RunwayDesignatorInput class

---
+++ src/main/java/com/example/segproject/components/RunwayDesignatorInput.java
@@ -0,0 +1,39 @@
+package com.example.segproject.components;
+
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.layout.HBox;
+
+/**
+ * An input for Runway Designators with format of an
+ * integer from 01 to 36 then optionally a
+ * choice from L, C or R
+ * <p>
+ * e.g. 14R, 35, 01L
+ */
+public class RunwayDesignatorInput extends HBox {
+    Spinner<Integer> spinner;
+    ChoiceBox<String> positionChoice;
+
+    public RunwayDesignatorInput() {
+        spinner = new Spinner<Integer>(1, 36, 1);
+        getChildren().add(spinner);
+
+        positionChoice = new ChoiceBox<>();
+        positionChoice.getItems().addAll("", "L", "C", "R");
+        getChildren().add(positionChoice);
+    }
+
+    /**
+     * Return the entered input for the runway designator
+     * in String form
+     * @return runway designator string
+     */
+    public String getText() {
+        String num = this.spinner.getValue().toString();
+        if (num.length()==1) {
+            num = "0".concat(num);
+        }
+        return num.concat(positionChoice.getValue());
+    }
+}
