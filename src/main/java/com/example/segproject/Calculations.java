b4e64559f295b0319347b32e6a2962c3bf181ec7
Patrik Csanyi <patriktibor@yahoo.com>
2022-03-07 13:34:50 +0000

    Calculations update, I lost my work, this is a test

--- src/main/java/com/example/segproject/Calculations.java
+++ src/main/java/com/example/segproject/Calculations.java
@@ -10,15 +10,28 @@ public class Calculations {
     int newAwayTODA;
     int newAwayLDA;
     String runwayName;
+    String obstacleDirection;
+    String obstacleDistanceFromCenter;
 
     public Calculations(String name,String tora, String asda, String toda, String lda, String height, String distance, String direction, String obstacleDistance, String displacement, String resa, String stripEnd, String blastProtection){
         runwayName = name;
+        obstacleDistanceFromCenter = obstacleDistance;
+        obstacleDirection = direction;
         int runway = Integer.parseInt(tora);
         int stopway = Integer.parseInt(asda) - runway;
         int clearway = Integer.parseInt(toda) - runway;
-        int slopeCalculation = 0; //Placeholder: max(RESA, height*50)
-        //Resa constant = 240?
-        //stripEnd constant = 60?
+        int slopeCalculation;
+        int buffer;
+        if (Integer.parseInt(height)*50 > Integer.parseInt(resa)){
+            slopeCalculation = Integer.parseInt(height)*50;
+        } else {
+            slopeCalculation = Integer.parseInt(resa);
+        }
+        if (Integer.parseInt(stripEnd) + slopeCalculation > Integer.parseInt(blastProtection)){
+            buffer = Integer.parseInt(stripEnd) + slopeCalculation;
+        } else {
+            buffer = Integer.parseInt(blastProtection);
+        }
         takeOffTowardsLandingTowards();
         takeOffAwayLandingOver(stopway, clearway);
     }
@@ -33,6 +46,6 @@ public class Calculations {
         newAwayTORA = 0; //Placeholder(so many questions)
         newAwayASDA = newAwayTORA + stopway;
         newAwayTODA = newAwayTORA + clearway;
-        newAwayLDA = 0; //Placeholder: lda - distance(from incoming direction) - slopeCalculation - max(stripEnd, blastProtectionValue)
+        newAwayLDA = 0; //Placeholder: lda - distance(from incoming direction) - max(slopeCalculation + stripEnd, blastProtectionValue)
      }
 }
