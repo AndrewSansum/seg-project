7d3a6d695e61b68dca3d350d4ddba76084a1df4c
Andrew Sansum <sansumandrew@gmail.com>
2022-03-09 17:44:20 +0000

    Add basic test cases for landing and taking off

--- src/test/java/com/example/segproject/CalculationsTest.java
+++ src/test/java/com/example/segproject/CalculationsTest.java
@@ -1,5 +1,24 @@
 package com.example.segproject;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class CalculationsTest {
-    
+    @Test
+    @DisplayName("Correct distances for takeoff")
+    public void testTakeOff() {
+        var calculator = new Calculations("01", "Takeoff", 3884, 3884, 3962, 3884, 12, 3646, "North", 0, 0, 240, 60, 300);
+        assertEquals(2986, calculator.getNewTORA(), "TORA is incorrect");
+        assertEquals(2986, calculator.getNewASDA(), "ASDA is incorrect");
+        assertEquals(2986, calculator.getNewTODA(), "TODA is incorrect");
+    }
+
+    @Test
+    @DisplayName("Correct distances for landing")
+    public void testLanding() {
+        var calculator = new Calculations("01", "Landing", 3884, 3884, 3962, 3884, 12, 3646, "North", 0, 0, 240, 60, 300);
+        assertEquals(3346, calculator.getNewLDA(), "LDA is incorrect");
+    }
 }
