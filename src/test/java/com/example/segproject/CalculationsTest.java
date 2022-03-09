d27812d0877d4ca39b0747e666889d2ea51399bc
Andrew Sansum <sansumandrew@gmail.com>
2022-03-09 21:38:00 +0000

    Parameterize calculations tests

--- src/test/java/com/example/segproject/CalculationsTest.java
+++ src/test/java/com/example/segproject/CalculationsTest.java
@@ -2,23 +2,35 @@ package com.example.segproject;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class CalculationsTest {
-    @Test
-    @DisplayName("Correct distances for takeoff")
-    public void testTakeOff() {
-        var calculator = new Calculations("01", "Takeoff", 3884, 3884, 3962, 3884, 12, 3646, "North", 0, 0, 240, 60, 300);
-        assertEquals(2986, calculator.getNewTORA(), "TORA is incorrect");
-        assertEquals(2986, calculator.getNewASDA(), "ASDA is incorrect");
-        assertEquals(2986, calculator.getNewTODA(), "TODA is incorrect");
+    private static Stream<Arguments> calculationsTestParamProvider() {
+        return Stream.of(
+            Arguments.of(3884, 3884, 3962, 3884, 12, 3646, 0, 2986, 2986, 2986, 3346)
+        );
     }
 
-    @Test
+    @ParameterizedTest
+    @MethodSource("calculationsTestParamProvider")
+    @DisplayName("Correct distances for takeoff")
+    public void testTakeOff(int tora, int asda, int toda, int lda, int obHeight, int obDistance, int displacement, int expTora, int expAsda, int expToda, int expLda) {
+        var calculator = new Calculations("01", "Takeoff", tora, asda, toda, lda, obHeight, obDistance, "North", 0, displacement, 240, 60, 300);
+        assertEquals(expTora, calculator.getNewTORA(), "TORA is incorrect");
+        assertEquals(expAsda, calculator.getNewASDA(), "ASDA is incorrect");
+        assertEquals(expToda, calculator.getNewTODA(), "TODA is incorrect");
+    }
+
+    @ParameterizedTest
+    @MethodSource("calculationsTestParamProvider")
     @DisplayName("Correct distances for landing")
-    public void testLanding() {
-        var calculator = new Calculations("01", "Landing", 3884, 3884, 3962, 3884, 12, 3646, "North", 0, 0, 240, 60, 300);
-        assertEquals(3346, calculator.getNewLDA(), "LDA is incorrect");
+    public void testLanding(int tora, int asda, int toda, int lda, int obHeight, int obDistance, int displacement, int expTora, int expAsda, int expToda, int expLda) {
+        var calculator = new Calculations("01", "Landing", tora, asda, toda, lda, obHeight, obDistance, "North", 0, displacement, 240, 60, 300);
+        assertEquals(expLda, calculator.getNewLDA(), "LDA is incorrect");
     }
 }
